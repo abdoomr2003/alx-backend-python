4f83f3178b564a4c902723c6c4486469eb2ae2b7
abdoomr2003 <abdoomer1112003@gmail.com>
2024-06-06 10:10:23 +0300

    importing typing to avoid quots

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""No-module imported"""
+"""Import List from typing"""
+from typing import List
 
 
-def sum_list(input_list: 'list[float]') -> float:
+def sum_list(input_list: List[float]) -> float:
     """ function sum_list which takes a list input_list of floats as argument
     and returns their sum as a float.
 
