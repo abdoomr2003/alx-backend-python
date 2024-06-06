84c8d765507b37591e91e66caf1ccc393c52935d
abdoomr2003 <abdoomer1112003@gmail.com>
2024-06-06 08:08:08 +0300

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def add(a: float, b: float) -> float:
+    """function takes a float a and a float b as arguments and returns their sum as a float.
+
+    Args:
+        a (float): given number from user.
+        b (float): given number from user.
+
+    Returns:
+        float: retuen number to user
+    """
+    return a + b
